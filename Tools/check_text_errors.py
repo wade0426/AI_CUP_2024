9e8e30cc55a9c879287b4b3320d8cb3bad67563f
Lian <laix678@gmail.com>
2024-10-20 00:16:24 +0800

    修正_工具_check_text_errors.py

--- Tools/check_text_errors.py
+++ Tools/check_text_errors.py
@@ -118,62 +118,78 @@ if __name__ == "__main__":
 
     # path = "D:\\NTCUST\\Project\\Competition\\AI_CUP\\AI_CUP_2024\\finance_301-400\\text\\"
     
-    # path = "D:\\NTCUST\\Project\\Competition\\AI_CUP\\AI_CUP_2024\\finance_301-400\\text\\v1\\"
-    # os.chdir(path)
+    path = "D:\\NTCUST\\Project\\Competition\\AI_CUP\\AI_CUP_2024\\finance_301-400\\text\\v2\\"
+    os.chdir(path)
 
-    # # 用來計算平均值
-    # n_newline_list = []
-    # n_space_list = []
-    # # 基於文本相似度的檢查
-    # similar_text_list = []
+    # 用來計算平均值
+    n_newline_list = []
+    n_space_list = []
+    # 基於文本相似度的檢查
+    similar_text_list = []
 
-    # for text_path in os.listdir(path):
+    # 定義錯誤對比資料
+    error_comparison_list = [
+        "301_15.txt", "301_25.txt", "325_1.txt", "327_36.txt", "328_1.txt", "340_1.txt", "340_1.txt", "351_1.txt", 
+        "359_15.txt", "359_5.txt", "360_1.txt", "361_2.txt", "362_2.txt", "363_2.txt", "363_3.txt", "364_2.txt", 
+        "366_1.txt", "367_2.txt", "372_1.txt", "375_2.txt", "377_1.txt", "378_1.txt", "382_1.txt", "383_8.txt", 
+        "384_1.txt", "385_1.txt", "387_1.txt", "387_1.txt", "387_2.txt", "392_1.txt", "393_2.txt", "394_2.txt", 
+        "396_1.txt", "396_3.txt", "398_10.txt", "398_2.txt", "398_21.txt", "398_21.txt", "398_26.txt", "398_27.txt", 
+        "398_28.txt", "398_37.txt", "398_38.txt", "398_39.txt", "398_4.txt", "398_40.txt", "398_41.txt", "398_42.txt", 
+        "398_6.txt", "400_1.txt"
+    ]
 
-    #     # 開檔
-    #     with open(text_path, 'r', encoding='utf-8') as file:
-    #         text = file.read()
+    for text_path in os.listdir(path):
 
-    #     # print(os.getcwd())
+        # 開檔
+        with open(text_path, 'r', encoding='utf-8') as file:
+            text = file.read()
 
-    #     n_newline, n_space = check_text_errors(text)
-    #     n_newline_list.append(n_newline)
-    #     n_space_list.append(n_space)
+        # print(os.getcwd())
 
-    #     similar_text_list.append(len(find_similar_text(text)))
-    #     print(f"{text_path},換行: {n_newline}, 空白: {n_space}, 相似文本: {len(find_similar_text(text))}")
+        n_newline, n_space = check_text_errors(text)
+        n_newline_list.append(n_newline)
+        n_space_list.append(n_space)
 
-    # newline_average = sum(n_newline_list) / len(n_newline_list)
-    # space_average = sum(n_space_list) / len(n_space_list)
-    # similar_text_average = sum(similar_text_list) / len(similar_text_list)
+        similar_text_list.append(len(find_similar_text(text)))
+        print(f"{text_path},換行: {n_newline}, 空白: {n_space}, 相似文本: {len(find_similar_text(text))}")
 
-    # print(f"\n\n換行平均: {newline_average}")
-    # print(f"空白平均: {space_average}")
-    # print(f"相似文本平均: {similar_text_average}")
-    # # 設定閥值
-    # n_newline_threshold = newline_average*1.7
-    # n_space_threshold = space_average*1.8
-    # similar_text_threshold = similar_text_average*3.38
-    # space_bad_text_list = []
-    # similar_bad_text_list = []
+    newline_average = sum(n_newline_list) / len(n_newline_list)
+    space_average = sum(n_space_list) / len(n_space_list)
+    similar_text_average = sum(similar_text_list) / len(similar_text_list)
 
-    # for text_path in os.listdir(path):
+    print(f"\n\n換行平均: {newline_average}")
+    print(f"空白平均: {space_average}")
+    print(f"相似文本平均: {similar_text_average}")
+    # 設定閥值
+    n_newline_threshold = newline_average*1.7
+    n_space_threshold = space_average*1.8
+    similar_text_threshold = similar_text_average*2.9
+    space_bad_text_list = []
+    similar_bad_text_list = []
 
-    #     # 開檔
-    #     with open(text_path, 'r', encoding='utf-8') as file:
-    #         text = file.read()
+    for text_path in os.listdir(path):
 
-    #     n_newline, n_space = check_text_errors(text)
-    #     if n_space > n_space_threshold:
-    #         space_bad_text_list.append(text_path)
+        # 開檔
+        with open(text_path, 'r', encoding='utf-8') as file:
+            text = file.read()
+
+        n_newline, n_space = check_text_errors(text)
+        if n_space > n_space_threshold:
+            space_bad_text_list.append(text_path)
         
-    #     if len(find_similar_text(text)) > similar_text_threshold:
-    #         similar_bad_text_list.append(text_path)
+        if len(find_similar_text(text)) > similar_text_threshold:
+            similar_bad_text_list.append(text_path)
 
     # print(f"根據空白閥值，可能壞檔案: {space_bad_text_list}")
-    # print(f"\n根據相似文本閥值，可能壞檔案: {similar_bad_text_list}")
+    print(f"\n根據相似文本閥值，可能壞檔案: {similar_bad_text_list}")
     # print(f"\n聯集: {space_bad_text_list + similar_bad_text_list}")
 
-
+    # similar_bad_text_list 和 error_comparison_list 比較 看正確率
+    correct = 0
+    for i in similar_bad_text_list:
+        if i in error_comparison_list:
+            correct += 1
+    print(f"\n相似文本閥值，可能壞檔案正確率: {correct/len(similar_bad_text_list)}")
 
     # for text_path in os.listdir(path):
 
@@ -186,20 +202,20 @@ if __name__ == "__main__":
 
     # print(f"根據相似文本閥值，可能壞檔案: {similar_bad_text_list}")
 
-    path = "D:\\NTCUST\\Project\\Competition\\AI_CUP\\AI_CUP_2024\\finance_0-100\\text\\v2\\"
-    os.chdir(path)
+#     path = "D:\\NTCUST\\Project\\Competition\\AI_CUP\\AI_CUP_2024\\finance_0-100\\text\\v2\\"
+#     os.chdir(path)
 
-    # 0.21高
-    with open("4_1.txt", 'r', encoding='utf-8') as file:
-        file_content = file.read()
+#     # 0.21高
+#     with open("4_1.txt", 'r', encoding='utf-8') as file:
+#         file_content = file.read()
 
-    print()
-    print(check_similarity_return_similarity("""| 项目 | 112年3月31日 | 111年12月31日 | 111年3月31日 |
-|------|--------------|---------------|--------------|
-| 庫存现金 | $ 1,777 | $ 1,750 | $ 2,137 |
-| 活期存款 | 25,699,639 | 33,243,220 | 39,578,483 |
-| 总计 | $ 77,674,551 | $ 91,065,529 | $ 68,517,225 |""", file_content))
-    print()
+#     print()
+#     print(check_similarity_return_similarity("""| 项目 | 112年3月31日 | 111年12月31日 | 111年3月31日 |
+# |------|--------------|---------------|--------------|
+# | 庫存现金 | $ 1,777 | $ 1,750 | $ 2,137 |
+# | 活期存款 | 25,699,639 | 33,243,220 | 39,578,483 |
+# | 总计 | $ 77,674,551 | $ 91,065,529 | $ 68,517,225 |""", file_content))
+#     print()
     
     # 0.159
     # similarity, max_similarity_string = check_similarity("""输入图像描述：财务报告页面的图像，包含""", file_content)
