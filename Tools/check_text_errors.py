d6a8a8d2236de90e0a1d1f2abedcc64a88f69efa
Lian <laix678@gmail.com>
2024-10-23 21:32:12 +0800

    修正_工具_檢查錯誤文本
    ACC rate:0.7

--- Tools/check_text_errors.py
+++ Tools/check_text_errors.py
@@ -114,93 +114,109 @@ def check_similarity_return_similarity(input_string, file_content):
     return cosine_sim[0][1]
 
 
-if __name__ == "__main__":
+# 查看正確率 傳入 可能壞檔案 和 人工檢驗的錯誤資料
+def check_correct_rate(bad_text_list=[], error_comparison_list=[]):
 
-    # path = "D:\\NTCUST\\Project\\Competition\\AI_CUP\\AI_CUP_2024\\finance_301-400\\text\\"
-    
-    path = "D:\\NTCUST\\Project\\Competition\\AI_CUP\\AI_CUP_2024\\finance_301-400\\text\\v2\\"
-    os.chdir(path)
+    # 定義錯誤對比資料
+    # error_comparison_list = [
+    #     "301_15.txt", "301_25.txt", "325_1.txt", "327_36.txt", "328_1.txt", "340_1.txt", "340_1.txt", "351_1.txt", 
+    #     "359_15.txt", "359_5.txt", "360_1.txt", "361_2.txt", "362_2.txt", "363_2.txt", "363_3.txt", "364_2.txt", 
+    #     "366_1.txt", "367_2.txt", "372_1.txt", "375_2.txt", "377_1.txt", "378_1.txt", "382_1.txt", "383_8.txt", 
+    #     "384_1.txt", "385_1.txt", "387_1.txt", "387_1.txt", "387_2.txt", "392_1.txt", "393_2.txt", "394_2.txt", 
+    #     "396_1.txt", "396_3.txt", "398_10.txt", "398_2.txt", "398_21.txt", "398_21.txt", "398_26.txt", "398_27.txt", 
+    #     "398_28.txt", "398_37.txt", "398_38.txt", "398_39.txt", "398_4.txt", "398_40.txt", "398_41.txt", "398_42.txt", 
+    #     "398_6.txt", "400_1.txt"
+    # ]
 
+    # similar_bad_text_list 和 error_comparison_list 比較 看正確率
+    # correct = 0
+    # for i in similar_bad_text_list:
+    #     if i in error_comparison_list:
+    #         correct += 1
+    # print(f"\n相似文本閥值，可能壞檔案正確率: {correct/len(similar_bad_text_list)}")
+
+    correct = 0
+    for i in bad_text_list:
+        if i in error_comparison_list:
+            correct += 1
+    return correct/len(bad_text_list)
+
+
+def main():
     # 用來計算平均值
     n_newline_list = []
     n_space_list = []
     # 基於文本相似度的檢查
     similar_text_list = []
+    return_All_bad_text_list = []
+    
+    folder_list = ["finance_0-100", "finance_101-200", "finance_201-300", "finance_301-400", "finance_401-500", "finance_501-600", "finance_601-700", "finance_701-800", "finance_801-900", "finance_901-1034"]
 
-    # 定義錯誤對比資料
-    error_comparison_list = [
-        "301_15.txt", "301_25.txt", "325_1.txt", "327_36.txt", "328_1.txt", "340_1.txt", "340_1.txt", "351_1.txt", 
-        "359_15.txt", "359_5.txt", "360_1.txt", "361_2.txt", "362_2.txt", "363_2.txt", "363_3.txt", "364_2.txt", 
-        "366_1.txt", "367_2.txt", "372_1.txt", "375_2.txt", "377_1.txt", "378_1.txt", "382_1.txt", "383_8.txt", 
-        "384_1.txt", "385_1.txt", "387_1.txt", "387_1.txt", "387_2.txt", "392_1.txt", "393_2.txt", "394_2.txt", 
-        "396_1.txt", "396_3.txt", "398_10.txt", "398_2.txt", "398_21.txt", "398_21.txt", "398_26.txt", "398_27.txt", 
-        "398_28.txt", "398_37.txt", "398_38.txt", "398_39.txt", "398_4.txt", "398_40.txt", "398_41.txt", "398_42.txt", 
-        "398_6.txt", "400_1.txt"
-    ]
+    for folder in folder_list:
+        path = f"D:\\NTCUST\\Project\\Competition\\AI_CUP\\AI_CUP_2024\\{folder}\\text\\v4\\"
+        os.chdir(path)
 
-    for text_path in os.listdir(path):
+        for text_path in os.listdir(path):
 
-        # 開檔
-        with open(text_path, 'r', encoding='utf-8') as file:
-            text = file.read()
+            # 開檔
+            with open(text_path, 'r', encoding='utf-8') as file:
+                text = file.read()
 
-        # print(os.getcwd())
+            # print(os.getcwd())
 
-        n_newline, n_space = check_text_errors(text)
-        n_newline_list.append(n_newline)
-        n_space_list.append(n_space)
+            n_newline, n_space = check_text_errors(text)
+            n_newline_list.append(n_newline)
+            n_space_list.append(n_space)
 
-        similar_text_list.append(len(find_similar_text(text)))
-        print(f"{text_path},換行: {n_newline}, 空白: {n_space}, 相似文本: {len(find_similar_text(text))}")
+            similar_text_list.append(len(find_similar_text(text)))
+            print(f"{text_path},換行: {n_newline}, 空白: {n_space}, 相似文本: {len(find_similar_text(text))}")
 
-    newline_average = sum(n_newline_list) / len(n_newline_list)
-    space_average = sum(n_space_list) / len(n_space_list)
-    similar_text_average = sum(similar_text_list) / len(similar_text_list)
+        newline_average = sum(n_newline_list) / len(n_newline_list)
+        space_average = sum(n_space_list) / len(n_space_list)
+        similar_text_average = sum(similar_text_list) / len(similar_text_list)
 
-    print(f"\n\n換行平均: {newline_average}")
-    print(f"空白平均: {space_average}")
-    print(f"相似文本平均: {similar_text_average}")
-    # 設定閥值
-    n_newline_threshold = newline_average*1.7
-    n_space_threshold = space_average*1.8
-    similar_text_threshold = similar_text_average*2.9
-    space_bad_text_list = []
-    similar_bad_text_list = []
+        print(f"\n\n換行平均: {newline_average}")
+        print(f"空白平均: {space_average}")
+        print(f"相似文本平均: {similar_text_average}")
+        # 設定閥值
+        n_newline_threshold = newline_average*1.7
+        n_space_threshold = space_average*4
+        similar_text_threshold = similar_text_average*11.6
+        # 可能壞檔案 空白
+        space_bad_text_list = []
+        # 可能壞檔案 相似文本
+        similar_bad_text_list = []
 
-    for text_path in os.listdir(path):
+        for text_path in os.listdir(path):
 
-        # 開檔
-        with open(text_path, 'r', encoding='utf-8') as file:
-            text = file.read()
+            # 開檔
+            with open(text_path, 'r', encoding='utf-8') as file:
+                text = file.read()
 
-        n_newline, n_space = check_text_errors(text)
-        if n_space > n_space_threshold:
-            space_bad_text_list.append(text_path)
-        
-        if len(find_similar_text(text)) > similar_text_threshold:
-            similar_bad_text_list.append(text_path)
+            n_newline, n_space = check_text_errors(text)
+            if n_space > n_space_threshold:
+                space_bad_text_list.append(text_path)
+            
+            if len(find_similar_text(text)) > similar_text_threshold:
+                similar_bad_text_list.append(text_path)
 
-    # print(f"根據空白閥值，可能壞檔案: {space_bad_text_list}")
-    print(f"\n根據相似文本閥值，可能壞檔案: {similar_bad_text_list}")
-    # print(f"\n聯集: {space_bad_text_list + similar_bad_text_list}")
+        # print(f"根據空白閥值，可能壞檔案: {space_bad_text_list}\n")
+        # print(f"根據相似文本閥值，可能壞檔案: {similar_bad_text_list}\n")
+        print(f"\n聯集: {space_bad_text_list + similar_bad_text_list}")
 
-    # similar_bad_text_list 和 error_comparison_list 比較 看正確率
-    correct = 0
-    for i in similar_bad_text_list:
-        if i in error_comparison_list:
-            correct += 1
-    print(f"\n相似文本閥值，可能壞檔案正確率: {correct/len(similar_bad_text_list)}")
+        return_All_bad_text_list.extend(space_bad_text_list + similar_bad_text_list)
+    
+    # 將聯集 list 使用 py 存檔
+    output_path = "D:\\NTCUST\\Project\\Competition\\AI_CUP\\AI_CUP_2024"
+    with open(f"{output_path}\\All_bad_text_list.py", 'w', encoding='utf-8') as file:
+        file.write(f"bad_text_list = {return_All_bad_text_list}")
 
-    # for text_path in os.listdir(path):
 
-    #     # 開檔
-    #     with open(text_path, 'r', encoding='utf-8') as file:
-    #         text = file.read()
+if __name__ == "__main__":
 
-    #     if len(find_similar_text(text)) > similar_text_threshold:
-    #         similar_bad_text_list.append(text_path)
+    main()
 
-    # print(f"根據相似文本閥值，可能壞檔案: {similar_bad_text_list}")
+    
 
 #     path = "D:\\NTCUST\\Project\\Competition\\AI_CUP\\AI_CUP_2024\\finance_0-100\\text\\v2\\"
 #     os.chdir(path)
