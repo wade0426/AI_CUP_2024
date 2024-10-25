a202910778a32c137d70e7932f4959d6536da5a9
Lian <laix678@gmail.com>
2024-10-26 00:32:44 +0800

    完整 txt

--- All_bad_text_list.py
+++ All_bad_text_list.py
@@ -1,1 +1,3 @@
-bad_text_list = ['38_1.txt', '229_1.txt', '345_1.txt']
+bad_text_list = ['39_2.txt', '8_25.txt', '123_1.txt', '174_1.txt', '268_1.txt', '289_1.txt', '301_10.txt', '387_1.txt', '398_34.txt', '552_5.txt', '562_1.txt', '595_2.txt', '625_2.txt', '719_1.txt', '724_1.txt', '741_1.txt', '804_5.txt', '831_1.txt']
+
+print(len(bad_text_list))
